3b8bdd15ce3ca08a782b7d3fa6bbd074c5687998
Marcos Eduardo Brisson Ruiz <marcos.ruiz@certintra.com.br>
2020-02-04 14:19:53 -0300

    exercício de requisição ajax

--- assets/scripts/exercicio-03.js
+++ assets/scripts/exercicio-03.js
@@ -1,3 +1,4 @@
+// CÓDIGO PARA MÉTODO POST => ENVIA INFORMAÇÕES DA TELA DO FRONT PARA O BACK-END
 
 let nome = document.querySelector('#nome');
 let job = document.querySelector('#job');
@@ -20,9 +21,35 @@ form.addEventListener('submit', function (event) {
             return response.json();
         })
         .then(function (response) {
-            console.log(response);
+            // console.log(response);
             console.log('Cadastro efetuado com sucesso');
         });
 
     console.log(dados)
+});
+
+
+
+// CÓDIGO PARA MÉTODO GET => SOLICITA INFORMAÇÕES DO BACK END E EXIBE NA TELA DO FRONT
+
+let btn = document.querySelector('#load');
+let lista = document.querySelector('#list');
+
+btn.addEventListener('click', function () {
+    fetch('https://reqres.in/api/users?page=2')
+        .then(function (response) {
+            // console.log(response);
+            return response.json()
+        })
+        .then(function (response) {
+            response.data.forEach(function (user) {
+                // console.log(user);
+                let item = document.createElement('li');
+                item.classList.add('list-group-item');
+
+                item.innerHTML = '<img src="'+user.avatar+'"/> <span>'+user.first_name+'</span>';
+
+                list.appendChild(item);
+            })
+        })
 });
