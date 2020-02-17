d9e79d87a1c6fd2118042480c1c180ecb0620b0c
Marcos Eduardo Brisson Ruiz <marcos.ruiz@certintra.com.br>
2020-02-17 14:30:05 -0300

    Exercício de template string

---
+++ js-tdd-course/assets/scripts/exercicio-03-template-string.js
@@ -0,0 +1,43 @@
+
+let content = document.querySelector('.alchemy')
+
+const alchemy = {
+    title: 'O CAIBALION: OS 7 PRINCÍPIOS DA VERDADE',
+    intro: 'Os Princípios da Verdade são Sete; aquele que os conhece perfeitamente, possui a Chave Mágica com a qual todas as Portas do Templo podem ser abertas completamente.',
+    present: 'Os Sete Princípios em que se baseia toda a Filosofia hermética são os seguintes:',
+    precepts: [
+        'O Princípio de Mentalismo.',
+        'O Princípio de Correspondência.',
+        'O Princípio de Vibração.',
+        'O Princípio de Polaridade.',
+        'O Princípio de Ritmo.',
+        'O Princípio de Causa e Efeito.',
+        'O Princípio de Gênero.'
+    ],
+    conclusion: 'Estes Sete Princípios podem ser explicados e explanados, como vamos fazer nesta lição. Uma pequena explanação de cada um deles pode ser feita agora, e é o que vamos fazer.'
+}
+
+// console.log(alchemy.precepts)
+
+const markup = `
+
+   <article>
+        <header>
+            <h1>${alchemy.title}</h1>
+        </header>
+        <section class="p-5 mb-3">
+            <p>${alchemy.intro}</p>
+            <h2>${alchemy.present}</h2>
+        </section>
+        <section class="p-5 mb-3">
+            <ol>
+                ${alchemy.precepts.map((precept) => `<li>${precept}</li>`).join('')}
+            </ol>
+        </section>
+        <section class="p-5 mb-3">
+            <h6>${alchemy.conclusion}</h6>
+        </section>
+    </article>
+`
+
+content.innerHTML = markup
